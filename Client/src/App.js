61aeab92c08de408ac7fa2ad529f11bf26d097f7
Brian  Hess <brianhess@durhambrands.com>
2023-11-13 15:01:35 -0700

    Working on adding Form Validation

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Button, Card, Form, Container, Row, Col, Badge, Image, InputGroup, FormControl, } from 'react-bootstrap';
+import { Button, Card, Form, Container, Row, Col, Image, Alert } from 'react-bootstrap';
 import $, { data } from "jquery";
 import moment from 'moment';
+import { Formik, useFormik } from "formik"
 
 <script src="https://cdn.jsdelivr.net/npm/ajax-client@2.0.2/lib/ajax-client.min.js"></script>
 
@@ -20,9 +21,9 @@ function App() {
 
   };
 
-  $( document ).ready(function() {
+  $(document).ready(function () {
     document.getElementById("currentDate").innerHTML = new moment().format('llll'); // Sat, Nov 11, 2023 8:07 AM
-});
+  });
 
   const PrintTimer = () => {
 
@@ -47,9 +48,44 @@ function App() {
 
     var caseEach = document.getElementById("caseEachID").value;
     document.getElementById("eachBarcode").src = "https://barcode.orcascan.com/?data=" + caseEach;
-    
-
   }
+
+
+  const validate = values => {
+    const errors = {}
+    if (!values.item) {
+      errors.item = 'Required'
+    } else if (values.item.lenght < 4) {
+      errors.item = 'Must Enter Valid Item Number'
+    }
+
+    if (!values.case) {
+      errors.case = 'Required'
+    } else if (values.case.lenght < 1) {
+      errors.case = 'Must Enter Valid Case Count'
+    }
+
+    if (!values.caseQTY) {
+      errors.caseQTY = 'Required'
+    } else if (values.item.lenght < 1) {
+      errors.caseQTY = 'Must Enter Valid Case QTY'
+    }
+
+    return errors;
+  }
+
+  const formik = useFormik({
+    initialValues: {
+      item: '',
+      case: '',
+      caseQTY: ''
+    },
+    validate,
+    onSubmit: values => {
+      console.log("Formik fired.")
+    }
+  });
+
 
   return (
     <div className="App">
@@ -60,26 +96,29 @@ function App() {
 
           <Card className='mb-1' style={{ color: "#000" }}>
             <Card.Title className='fs-1 mb-1'>License Plate</Card.Title>
-            <Form>
+            <Form onSubmit={formik.handleSubmit}>
               <Form.Group controlId='formLicensePlate2'>
                 <Row>
                   <Col>
                     <Form.Label className='mb-1'>Item Number:</Form.Label>
-                    <Form.Control id='itemNumberID' className='text-center mb-1' type='text' placeholder='Enter Item Number' onChange={Controller}></Form.Control>
+                    <Form.Control id='itemNumberID' name='item' className='text-center mb-1' type='text' placeholder='Enter Item Number' onBlur={formik.values.item} value={formik.values.item} onChange={formik.handleChange} ></Form.Control>
+                    {formik.errors.item ? <div>{formik.errors.item}</div>: null}
 
                     <Image id='itemBarcode' className='img-fluid h-50 w-50' src={"https://barcode.orcascan.com/?data=EnterItemNumber"} onChange={Controller} rounded />
 
                   </Col>
                   <Col>
                     <Form.Label className='mb-1'>Case Count:</Form.Label>
-                    <Form.Control id='caseCountID' className='text-center mb-1' type='text' placeholder='Enter Case Count' onChange={Controller}></Form.Control>
+                    <Form.Control id='caseCountID' name='case' className='text-center mb-1' type='text' placeholder='Enter Case Count' value={formik.values.case} onChange={formik.handleChange}></Form.Control>
+                    {formik.errors.case ? <div>{formik.errors.case}</div>: null}
                     <Image id='caseBarcode' className='img-fluid h-50 w-50' src={"https://barcode.orcascan.com/?data=EnterCaseNumber"} onChange={Controller} rounded />
 
                   </Col>
                 </Row>
 
                 <Form.Label className='mb-1'>Case QTY</Form.Label>
-                <Form.Control id='Testing' className='text-center mb-1' type='text' placeholder='Case QTY' onChange={Controller} ></Form.Control>
+                <Form.Control id='Testing' name='caseQTY' className='text-center mb-1' type='text' placeholder='Case QTY' value={formik.values.caseQTY} onChange={formik.handleChange} ></Form.Control>
+                {formik.errors.caseQTY ? <div>{formik.errors.caseQTY}</div>: null}
 
                 <Row>
 
@@ -91,24 +130,29 @@ function App() {
                   </Col>
                 </Row>
 
-                <Container>
-                  <Button id='printButton' className='mb-5' onClickCapture={onClickCaptureHandler}>Print</Button>
-                </Container>
+
 
                 <Row>
                   <Col>
-                  <Form.Control className='text-center mb-5' placeholder='Enter Name' type='text' plaintext></Form.Control>
+                    <Form.Control className='text-center mb-5' placeholder='Enter Name' type='text' plaintext></Form.Control>
+
+                  </Col>
+                  <Col>
+
+                    <Container>
+                      <Button id='printButton' className='mb-5' type='submit' onClickCapture={onClickCaptureHandler}>Print</Button>
+                    </Container>
 
                   </Col>
 
                   <Col>
-                  <p id='currentDate' value="">Date goes here</p>
+                    <p id='currentDate' value="">Date goes here</p>
 
                   </Col>
                 </Row>
 
-                
-                
+
+
 
               </Form.Group>
             </Form>
