44b3cfb344682c26e307fec0e435b9e2f90ae42a
Brian  Hess <brianhess@durhambrands.com>
2023-11-14 10:22:21 -0700

    Fixed it!! Made all fields required updated the design no more borders and fits on one page

--- Client/src/App.js
+++ Client/src/App.js
@@ -12,63 +12,50 @@ import { Formik, useFormik } from "formik"
 function App() {
 
 
-  const onClickCaptureHandler = () => {
-
-    var div = document.getElementById('printButton');
-    div.style.display = 'none';
-
-    setTimeout(PrintTimer, 100);
-
-  };
-
   $(document).ready(function () {
     document.getElementById("currentDate").innerHTML = new moment().format('llll'); // Sat, Nov 11, 2023 8:07 AM
   });
 
   const PrintTimer = () => {
-
     window.print();
-
   };
-
-  function Controller() {
-
-    var itemCount = document.getElementById("itemNumberID").value;
-    document.getElementById("itemBarcode").src = "https://barcode.orcascan.com/?data=" + itemCount;
-
-    var caseCount = document.getElementById("caseCountID").value;
-    document.getElementById("caseBarcode").src = "https://barcode.orcascan.com/?data=" + caseCount;
-
-    var Calculator = document.getElementById("Testing").value;
-
-    var Results = Calculator * caseCount;
-    document.getElementById("caseEachID").value = Results;
-
-    Results = caseEach;
-
-    var caseEach = document.getElementById("caseEachID").value;
-    document.getElementById("eachBarcode").src = "https://barcode.orcascan.com/?data=" + caseEach;
-  }
-
 
   const validate = values => {
     const errors = {}
     if (!values.item) {
       errors.item = 'Required'
-    } else if (values.item.lenght < 4) {
-      errors.item = 'Must Enter Valid Item Number'
+      
+    }else if(values.item){
+      var itemCount = document.getElementById("itemNumberID").value;
+    document.getElementById("itemBarcode").src = "https://barcode.orcascan.com/?data=" + itemCount;
+
     }
 
     if (!values.case) {
       errors.case = 'Required'
-    } else if (values.case.lenght < 1) {
-      errors.case = 'Must Enter Valid Case Count'
+    }else if(values.case){
+      var caseCount = document.getElementById("caseCountID").value;
+    document.getElementById("caseBarcode").src = "https://barcode.orcascan.com/?data=" + caseCount;
+
     }
 
     if (!values.caseQTY) {
       errors.caseQTY = 'Required'
-    } else if (values.item.lenght < 1) {
-      errors.caseQTY = 'Must Enter Valid Case QTY'
+    }else if(values.caseQTY){
+      var Calculator = document.getElementById("Testing").value;
+
+      var Results = Calculator * caseCount;
+      document.getElementById("caseEachID").value = Results;
+  
+      Results = caseEach;
+  
+      var caseEach = document.getElementById("caseEachID").value;
+      document.getElementById("eachBarcode").src = "https://barcode.orcascan.com/?data=" + caseEach;
+
+    }
+
+    if (!values.userName) {
+      errors.userName = 'Required'
     }
 
     return errors;
@@ -78,11 +65,15 @@ function App() {
     initialValues: {
       item: '',
       case: '',
-      caseQTY: ''
+      caseQTY: '',
+      userName: ''
     },
     validate,
     onSubmit: values => {
-      console.log("Formik fired.")
+      var div = document.getElementById('printButton');
+      div.style.display = 'none';
+  
+      setTimeout(PrintTimer, 100);
     }
   });
 
@@ -94,66 +85,54 @@ function App() {
 
         <Container fluid className='mr-auto p-2'>
 
-          <Card className='mb-1' style={{ color: "#000" }}>
-            <Card.Title className='fs-1 mb-1'>License Plate</Card.Title>
+          <Card className='mb-1' border='0' style={{ color: "#000" }}>
+            <Card.Title className='fs-1'>License Plate</Card.Title>
             <Form onSubmit={formik.handleSubmit}>
               <Form.Group controlId='formLicensePlate2'>
                 <Row>
                   <Col>
-                    <Form.Label className='mb-1'>Item Number:</Form.Label>
-                    <Form.Control id='itemNumberID' name='item' className='text-center mb-1' type='text' placeholder='Enter Item Number' onBlur={formik.values.item} value={formik.values.item} onChange={formik.handleChange} ></Form.Control>
-                    {formik.errors.item ? <div>{formik.errors.item}</div>: null}
-
-                    <Image id='itemBarcode' className='img-fluid h-50 w-50' src={"https://barcode.orcascan.com/?data=EnterItemNumber"} onChange={Controller} rounded />
-
+                    <Form.Label className=''>Item Number:</Form.Label>
+                    <Form.Control id='itemNumberID' name='item' className='text-center' type='text' placeholder='Enter Item Number' onBlur={formik.handleBlur} value={formik.values.item} onChange={formik.handleChange} plaintext ></Form.Control>
+                    {formik.touched.item && formik.errors.item ? <div>{formik.errors.item}</div>: null}
+                    <Image id='itemBarcode' className='img-fluid h-50 w-50' src={"https://barcode.orcascan.com/?data=EnterItemNumber"} onChange={formik.handleChange} rounded />
                   </Col>
-                  <Col>
-                    <Form.Label className='mb-1'>Case Count:</Form.Label>
-                    <Form.Control id='caseCountID' name='case' className='text-center mb-1' type='text' placeholder='Enter Case Count' value={formik.values.case} onChange={formik.handleChange}></Form.Control>
-                    {formik.errors.case ? <div>{formik.errors.case}</div>: null}
-                    <Image id='caseBarcode' className='img-fluid h-50 w-50' src={"https://barcode.orcascan.com/?data=EnterCaseNumber"} onChange={Controller} rounded />
-
-                  </Col>
-                </Row>
-
-                <Form.Label className='mb-1'>Case QTY</Form.Label>
-                <Form.Control id='Testing' name='caseQTY' className='text-center mb-1' type='text' placeholder='Case QTY' value={formik.values.caseQTY} onChange={formik.handleChange} ></Form.Control>
-                {formik.errors.caseQTY ? <div>{formik.errors.caseQTY}</div>: null}
-
-                <Row>
 
                   <Col>
-                    <Form.Label className='mb-1'>Each's</Form.Label>
-                    <Form.Control id='caseEachID' className='text-center mb-5' type='text' onChange={Controller} disabled plaintext></Form.Control>
-                    <Image id='eachBarcode' className='img-fluid h-50 w-50' src={"https://barcode.orcascan.com/?data=EnterEachNumber"} onChange={Controller} rounded />
+                    <Form.Label className=''>Case Count:</Form.Label>
+                    <Form.Control id='caseCountID' name='case' className='text-center' type='text' placeholder='Enter Case Count' value={formik.values.case} onBlur={formik.handleBlur} onChange={formik.handleChange} plaintext></Form.Control>
+                    {formik.touched.case && formik.errors.case ? <div>{formik.errors.case}</div>: null}
+                    <Image id='caseBarcode' className='img-fluid h-50 w-50' src={"https://barcode.orcascan.com/?data=EnterCaseNumber"} onChange={formik.handleChange} rounded />
 
                   </Col>
-                </Row>
+        
+                <Form.Label className=''>Case QTY</Form.Label>
+                <Form.Control id='Testing' name='caseQTY' className='text-center' type='text' placeholder='Case QTY' value={formik.values.caseQTY} onBlur={formik.handleBlur} onChange={formik.handleChange} plaintext ></Form.Control>
+                {formik.touched.caseQTY && formik.errors.caseQTY ? <div>{formik.errors.caseQTY}</div>: null}
 
-
-
-                <Row>
                   <Col>
-                    <Form.Control className='text-center mb-5' placeholder='Enter Name' type='text' plaintext></Form.Control>
-
-                  </Col>
-                  <Col>
-
+                    <Form.Label className=''>Eaches:</Form.Label>
+                    <Form.Control id='caseEachID' className='text-center' type='text' onChange={formik.handleChange} disabled plaintext></Form.Control>
+                    
                     <Container>
-                      <Button id='printButton' className='mb-5' type='submit' onClickCapture={onClickCaptureHandler}>Print</Button>
+                    <Image id='eachBarcode' className='img-fluid h-25 w-50' src={"https://barcode.orcascan.com/?data=EnterEachNumber"} onChange={formik.handleChange} rounded />
+                    </Container>
+                    
+
+                  </Col>
+                    <Container>
+                      <Button id='printButton' className='' type='submit' onClickCapture={formik.handleSubmit}>Print</Button>
                     </Container>
 
-                  </Col>
+                    <Col>
+                    <Form.Control name='userName' className='text-center' placeholder='Enter Name' type='text'value={formik.values.userName} onBlur={formik.handleBlur} onChange={formik.handleChange} plaintext></Form.Control>
+                    {formik.touched.userName && formik.errors.userName ? <div>{formik.errors.userName}</div>: null}
+                    </Col>
 
-                  <Col>
+                    <Col>
                     <p id='currentDate' value="">Date goes here</p>
-
-                  </Col>
+                    </Col>
+                    
                 </Row>
-
-
-
-
               </Form.Group>
             </Form>
           </Card>
